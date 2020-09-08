b58b8e119baa93ea5bd8f2cb664b5b2c1e851bc7
Shaun Befort <shaun.befort@gomolearning.com>
2020-09-08 02:45:36 -0500

    Updated plugin dependencies

--- blocks/instilled_media_gallery/version.php
+++ blocks/instilled_media_gallery/version.php
@@ -1,5 +1,9 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020082408;
+$plugin->version   = 2020090101;
 $plugin->component = 'block_instilled_media_gallery';
+$plugin->dependencies = array(
+  'local_instilled_media_gallery' => 2020090101,
+  'mod_instilledvideo' => 2020090101,
+);
--- local/instilled_media_gallery/version.php
+++ local/instilled_media_gallery/version.php
@@ -3,6 +3,9 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_instilled_media_gallery';
 $plugin->release = 'beta1';
-$plugin->version = 2020082609;
+$plugin->version = 2020090101;
 $plugin->maturity = MATURITY_BETA;
-$plugin->dependencies = array();
+$plugin->dependencies = array(
+  'mod_instilledvideo' => 2020090101,
+  'block_instilled_media_gallery' => 2020090101,
+);
