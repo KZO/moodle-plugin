ca2b52d294c0495e174bc5831bf0fdb60c141261
Shaun Befort <shaun.befort@gomolearning.com>
2020-09-24 10:19:35 -0500

    Added language strings

--- mod/instilledvideo/lang/en/instilledvideo.php
+++ mod/instilledvideo/lang/en/instilledvideo.php
@@ -1,4 +1,27 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language strings for mod_instilledvideo
+ *
+ * @package   mod_instilledvideo
+ * @copyright 2020 Instilled <support@instilled.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 $string['modulename'] = 'Instilled Video';
 $string['modulenameplural'] = 'Instilled Videos';
 $string['pluginname'] = 'Instilled Video';
@@ -13,3 +36,22 @@ $string['getvideoviewstats'] = 'Get video view stats from Instilled';
 
 $string['youcanupload'] = 'You can upload videos in the';
 $string['instilledmediagallery'] = 'Instilled Media Gallery';
+
+$string['instilledvideo:addinstance'] = 'Add a new Instilled Video resource';
+$string['instilledvideo:view'] = 'View Instilled Video content';
+$string['instilledvideo:manage'] = 'Manage Instilled Video content';
+$string['instilledvideo:edit'] = 'Edit Instilled Video content';
+
+$string['privacy:metadata:externalpurpose'] = 'The Instilled Video provides user information to build the video.';
+$string['privacy:metadata:userid'] = 'The ID of the user accessing Instilled.';
+$string['privacy:metadata:username'] = 'The username of the user accessing Instilled';
+$string['privacy:metadata:useridnumber'] = 'The IDnumeber of the user accessing Instilled';
+$string['privacy:metadata:firstname'] = 'The Firstname of the user accessing Instilled';
+$string['privacy:metadata:lastname'] = 'The Lastname of the user accessing Instilled';
+$string['privacy:metadata:fullname'] = 'The Fullname of the course accessing Instilled';
+$string['privacy:metadata:email'] = 'The email of the user accessing Instilled';
+$string['privacy:metadata:role'] = 'The role of the user accessing Instilled';
+$string['privacy:metadata:custompublishdata'] = 'The custompublishdata of the user accessing Instilled';
+$string['privacy:metadata:editor'] = 'The editor of the user accessing Instilled';
+$string['privacy:metadata:module'] = 'The module of the user accessing Instilled';
+$string['privacy:metadata:source'] = 'The source of the user accessing Instilled';
