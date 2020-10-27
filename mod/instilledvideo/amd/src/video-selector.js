9029dd99c5cc5cde0b70d7526cf0d2634fcfe3e2
Shaun Befort <shaun.befort@gomolearning.com>
2020-10-27 09:03:46 -0500

    Solution to fix clashes with Drift plugin

--- mod/instilledvideo/amd/src/video-selector.js
+++ mod/instilledvideo/amd/src/video-selector.js
@@ -2,6 +2,12 @@ define(['jquery'], function($) {
   return {
     init: function() {
       window.addEventListener('message', event => {
+        if (typeof event.data === 'string' || event.data instanceof String) {
+          $('#id_mediumid').val(event.data);
+        }
+      });
+
+      window.addEventListener('instilledMessage', event => {
         $('#id_mediumid').val(event.data);
       });
     }
